36ae5f7e64b30bbac3f0919844f9feea201fccd7
KhanradCoder <akhanrade@gmail.com>
2024-01-11 11:00:10 -0500

    add upload functionality

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 import gemini_summarize
+from werkzeug.utils import secure_filename
+import whisper
+import os
 
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'static/audio'
 
 @app.route('/')
 def index():
@@ -12,6 +17,23 @@ def summary():
     transcript = request.form['transcript'] 
     summary = gemini_summarize.get_summary(transcript) 
     return render_template("index.html", summary=summary)
+
+@app.route('/upload', methods=['POST'])
+def upload_audio():
+    if 'audio_file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['audio_file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        model = whisper.load_model("base")
+        transcription = model.transcribe(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return render_template("index.html", summary="", transcription=transcription["text"])
+
 
 if __name__ == '__main__':
     app.run()
